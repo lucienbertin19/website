3686f1d83d67266eed9038dff550a1ac6da67723
lucien <lucienbertin19@gmail.com>
2019-01-28 00:08:54 +0000

    xxx

--- php/database.php
+++ php/database.php
@@ -8,7 +8,6 @@
             $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo 'connection successful';
             }
         catch(PDOException $e)
             {
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+   $d = strtotime("2019-1-1");
+   echo date('l', $d);
+?>
